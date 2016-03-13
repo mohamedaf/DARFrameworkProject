1a350c6f9fa2056d0ae411124642850a59ad3488
AFFES Mohamed <mohamed.af@hotmail.fr>
2016-03-13 14:11:57 +0100

    meilleur affichage des erreurs en html

--- DarServerApi/src/model/response/HttpResponseError.java
+++ DarServerApi/src/model/response/HttpResponseError.java
@@ -5,7 +5,15 @@ public class HttpResponseError {
     public static void setHttpResponseError(IHttpResponse resp, HttpResponseStatus status) {
 
 	resp.setStatus(status);
-	resp.setBody("Error " + status.getStatus() + " " + status.name());
+	if(resp.getHeaderValue(HeaderResponseField.CONTENT_TYPE).startsWith("text/html")) {
+	    String response = "<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/>"
+	    	+ "<title>Http Error</title></head><body><span style=\"color:red;\">"
+	    	+ "Error " + status.getStatus() + " " + status.name() + "</span></body></html>";
+	    resp.setBody(response);
+	}
+	else {
+	    resp.setBody("Error " + status.getStatus() + " " + status.name());
+	}
 
     }
 
